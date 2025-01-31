33355d3371c0097fc558c7f933b884f6ccbbb0dd
Cedric Ziel <cedric@cedric-ziel.com>
2025-01-31 13:16:40 +0100

    chore: adjust test

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -1,4 +1,4 @@
 it("should load the page", () => {
     cy.visit("/");
-    cy.findAllByText(/learn react/i).should("have.length", 1);
+    cy.findAllByText(/Home Assistant Advanced Automation/i).should("have.length", 1);
 });
